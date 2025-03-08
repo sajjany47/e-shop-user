fcb5770a0311b2cd6ce7471c726811d41847428e
sajjany47 <sajjany47@gmail.com>
2025-03-09 01:10:37 +0530

    Remove ProductView component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Navbar from "./component/Navbar";
 import ProductList from "./component/ProductList";
-import ProductView from "./component/ProductView";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <ProductView />
+
       <ProductList />
     </>
   );
