43f9d46cfcc46dbb3f5d3ff889b3b9784f1031a9
QbAnurag <anurag.patra@qbent.in>
2025-03-27 15:10:06 +0530

    Add Redux Toolkit and React-Redux for state management; implement persistence with Redux Persist

---
+++ src/store/store.ts
@@ -0,0 +1,35 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({});
+
+const persistReducers = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
